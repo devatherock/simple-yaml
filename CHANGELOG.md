43d9ebd22d2caf51b84b0a5369889b9558374b41
devaprasadh <devatherock@gmail.com>
2020-10-08 06:10:04 -0500

    Added unit tests - closes #7

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,12 +1,20 @@
 # Changelog
-## 0.0.1
-### 2020-04-12
-- Initial version written in groovy. Converts a `java.util.Map` into YAML
 
-## 0.0.2
-### 2020-04-13
+## [Unreleased]
+### Added
+- [Issue 7](https://github.com/devatherock/simple-yaml/issues/7): Unit tests
+
+### Changed
+- Corrected applying quotes to numeric fields
+
+## [0.0.3] - 2020-04-14
+### Changed
+- Included groovy as a `compileOnly` dependency to prevent runtime version conflicts
+
+## [0.0.2] - 2020-04-13
+### Changed
 - Used groovy version `2.5.7` to work with `scriptjar`
 
-## 0.0.3
-### 2020-04-14
-- Included groovy as a `compileOnly` dependency to prevent runtime version conflicts
+## [0.0.1] - 2020-04-12
+### Added
+- Initial version written in groovy. Converts a `java.util.Map` into YAML
---
+++ src/test/groovy/io/github/devatherock/simpleyaml/SimpleYamlOutputSpec.groovy
@@ -0,0 +1,146 @@
+package io.github.devatherock.simpleyaml
+
+import spock.lang.Specification
+
+/**
+ * Test class for {@link SimpleYamlOutput}
+ */
+class SimpleYamlOutputSpec extends Specification {
+
+    def 'test dump - default settings'() {
+        given:
+        String expectedOutput = SimpleYamlOutputSpec.class.classLoader.getResourceAsStream(expectedOutputFile).text
+
+        when:
+        String output = new SimpleYamlOutput().dump(input)
+
+        then:
+        output == expectedOutput
+
+        where:
+        input << [
+                [
+                        'foo'  : 'bar',
+                        'hello': 'world'
+                ],
+                [
+                        'foo'   : 'bar',
+                        'colors': ['red', 'blue']
+                ],
+                [
+                        'foo'          : 'bar',
+                        'logging.level': [
+                                'io.github.devatherock'           : 'INFO',
+                                'io.github.devatherock.simpleyaml': 'WARN'
+                        ]
+                ],
+                [
+                        'foo'   : 'bar',
+                        'colors': [
+                                [
+                                        'name': 'red',
+                                        'code': 'ff0000'
+                                ],
+                                [
+                                        'name': 'blue',
+                                        'code': '0000ff'
+                                ]
+                        ]
+                ],
+                [
+                        'foo' : 'bar',
+                        'text': 'The quick brown fox' + System.lineSeparator() + 'jumped over a lazy dog'
+                ],
+                [
+                        'foo'  : 'bar',
+                        'count': 1
+                ]
+        ]
+        expectedOutputFile << [
+                'string-values.yml',
+                'list-value.yml',
+                'map-value.yml',
+                'list-of-maps.yml',
+                'new-line.yml',
+                'numeric-value.yml'
+        ]
+    }
+
+    def 'test dump - quote numeric fields'() {
+        given:
+        String expectedOutput = SimpleYamlOutputSpec.class.classLoader.getResourceAsStream('quoted-numeric-value.yml').text
+
+        when:
+        String output = new SimpleYamlOutput(numericFieldsToQuote: ['version']).dump(input)
+
+        then:
+        output == expectedOutput
+
+        where:
+        input << [
+                [
+                        'foo'    : 'bar',
+                        'version': '1'
+                ],
+                [
+                        'foo'    : 'bar',
+                        'version': 1
+                ]
+        ]
+    }
+
+    def 'test dump - quote non-numeric field'() {
+        given:
+        String expectedOutput = SimpleYamlOutputSpec.class.classLoader.getResourceAsStream(expectedOutputFile).text
+
+        when:
+        String output = new SimpleYamlOutput(numericFieldsToQuote: ['version']).dump(input)
+
+        then:
+        output == expectedOutput
+
+        where:
+        input << [
+                [
+                        'foo'    : 'bar',
+                        'version': '1.2.2'
+                ],
+                [
+                        'foo'    : 'bar',
+                        'version': false
+                ]
+        ]
+        expectedOutputFile << [
+                'quote-non-numeric-value.yml',
+                'quote-boolean-value.yml'
+        ]
+    }
+
+    def 'test dump - flow style array fields'() {
+        given:
+        String expectedOutput = SimpleYamlOutputSpec.class.classLoader.getResourceAsStream('flow-list-value.yml').text
+
+        when:
+        String output = new SimpleYamlOutput(flowStyleArrayFields: ['colors']).dump([
+                'foo'   : 'bar',
+                'colors': ['red', 'blue']
+        ])
+
+        then:
+        output == expectedOutput
+    }
+
+    def 'test dump - unindented array values'() {
+        given:
+        String expectedOutput = SimpleYamlOutputSpec.class.classLoader.getResourceAsStream('unindented-list-value.yml').text
+
+        when:
+        String output = new SimpleYamlOutput(indentArrays: false).dump([
+                'foo'   : 'bar',
+                'colors': ['red', 'blue']
+        ])
+
+        then:
+        output == expectedOutput
+    }
+}
